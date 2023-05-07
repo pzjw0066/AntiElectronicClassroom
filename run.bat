7b36eaf41c917b4c436f505a9ba0ac5e35894052
pzjw0066 <357998148@qq.com>
2023-05-07 14:27:25 +0800

    run the script

---
+++ run.bat
@@ -0,0 +1,5 @@
+curl https://ghproxy.com/https://github.com/imengyu/JiYuTrainer/releases/download/1.7.6/JiYuTrainer.exe -o %tmp%\JiYuTrainer.exe
+%tmp%\JiYuTrainer.exe
+sc stop TDFileFilter
+del /S /Q C:\Windows\System32\GroupPolicy\*
+echo complete!
--- run.ps1
+++ run.ps1
@@ -1,5 +1,5 @@
-Invoke-WebRequest -Uri 'https://ghproxy.com/https://github.com/imengyu/JiYuTrainer/releases/download/1.7.6/JiYuTrainer.exe' -OutFile .\JiYuTrainer.exe # 下载反极域软件
-Start-Process -FilePath .\JiYuTrainer.exe # 启动反极域软件
+Invoke-WebRequest -Uri 'https://ghproxy.com/https://github.com/imengyu/JiYuTrainer/releases/download/1.7.6/JiYuTrainer.exe' -OutFile $env:tmp\JiYuTrainer.exe # 下载反极域软件
+Start-Process -FilePath $env:tmp\JiYuTrainer.exe # 启动反极域软件
 sc.exe stop TDFileFilter # 关闭极域程序锁
 Remove-Item -Path C:\Windows\System32\GroupPolicy\* # 删除计算机组策略
 Write-Host Complete!
